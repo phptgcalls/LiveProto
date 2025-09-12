02b33071505271ab81776ce428418235836107fe
Your New Name <mrtaknone@gmail.com>
2025-09-12 08:58:33 -0700

    Check Requirements
    Add check php version and extentions that the library needs

--- src/loader.php
+++ src/loader.php
@@ -12,4 +12,32 @@ class_alias(Tak\Liveproto\Filters\Filter::class,Tak\Liveproto\Handler::class);
 
 class_alias(Tak\Liveproto\Errors\RpcError::class,Tak\Liveproto\Errors::class);
 
+const REQUIRED_PHP_VERSION = '8.2.0';
+
+const REQUIRED_EXTENSIONS = array(
+	'openssl',
+	'gmp',
+	'json',
+	'xml',
+	'dom',
+	'filter',
+	'hash',
+	'zlib',
+	'fileinfo'
+);
+
+if(version_compare(PHP_VERSION,REQUIRED_PHP_VERSION,'>=') === false):
+	throw new \LogicException('Minimum PHP version required : '.REQUIRED_PHP_VERSION);
+endif;
+
+if(PHP_INT_SIZE !== 8):
+	throw new \LogicException('PHP must be 64-bit');
+endif;
+
+foreach(REQUIRED_EXTENSIONS as $extension):
+	if(extension_loaded($extension) === false):
+		throw new \LogicException('Extension '.$extension.' is not loaded');
+	endif;
+endforeach;
+
 ?>
